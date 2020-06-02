3a5e74358f1c67dc396e04a2309eb61c55a528f0
Danish Riaz <danishriaz0609@outlook.com>
2020-06-02 08:04:33 +0500

    Parsing Incoming Request

--- Working with Express/app.js
+++ Working with Express/app.js
@@ -1,19 +1,26 @@
-const http = require('http');
 const express = require('express');
+const bodyParse = require('body-parser');
 
 const app = express();
 
+app.use(bodyParse.urlencoded({ extended: false }));
+
 app.use('/add-product', (req, res, next) => {
-    console.log('Inside second Middleware !');
-    res.send('<h1>ADD PAGE</h1>');
+    console.log('Inside add-product Middleware !');
+    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>');
+    res.end();
+});
+
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-    console.log('Inside second Middleware !');
+    console.log('Inside / Middleware !');
     res.send('<h1>Hello From Express</h1>');
 });
 
-app.listen(3000, () => {
-    console.log('Server Running at locathost 3000');
-})
+app.listen(3000);
 
