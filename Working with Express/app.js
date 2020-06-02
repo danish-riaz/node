d9b2c62c8e06c546622d9171f1283f46cfbfca0e
Danish Riaz <danishriaz0609@outlook.com>
2020-06-02 08:08:14 +0500

    Limmiting Middleware Execution to Only post request

--- Working with Express/app.js
+++ Working with Express/app.js
@@ -10,9 +10,9 @@ app.use('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>');
     res.end();
 });
-
-
-app.use('/product', (req, res, next) => {
+// app.use run for both incoming 'GET' & 'POST' request
+//we can use app.get OR app.post to make it handle only one
+app.post('/product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
