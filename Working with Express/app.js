5333215b3e6c44e53453c1cecc9b95030169a32b
Danish Riaz <danishriaz0609@outlook.com>
2020-06-02 07:33:26 +0500

    removing node_modules

--- Working with Express/app.js
+++ Working with Express/app.js
@@ -3,26 +3,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Inside first Middleware !');
-    // res.send('<h1>Inside first Middleware !</h1>');
-    // return res.end();
-    next(); // for jumping into the next middleware
+app.use('/add-product', (req, res, next) => {
+    console.log('Inside second Middleware !');
+    res.send('<h1>ADD PAGE</h1>');
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('Inside second Middleware !');
-    // Content-Type will be set by express (will auto detect type)
-    // offcourse we can always overwrite.
     res.send('<h1>Hello From Express</h1>');
 });
 
-// const server = http.createServer(app);
-// server.listen(3000, () => {
-//     console.log('Server Running at locathost 3000');
-// });
-
-// The above code is packed in a single express funciton
 app.listen(3000, () => {
     console.log('Server Running at locathost 3000');
 })
