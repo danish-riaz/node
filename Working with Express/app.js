a4cf1e336ab1569eb5fd359f5daed1fb921ad7db
Danish Riaz <danishriaz0609@outlook.com>
2020-06-02 08:52:39 +0500

    Handling 404 error page

--- Working with Express/app.js
+++ Working with Express/app.js
@@ -11,5 +11,10 @@ app.use(bodyParse.urlencoded({ extended: false }));
 app.use(adminRoute);
 app.use(shopRoute);
 
+// '/' is the default route if not specified
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
 app.listen(3000);
 
