7cf86f692b4eed89407b1af8c7e6139f551637fd
Danish Riaz <danishriaz0609@outlook.com>
2020-06-02 09:33:46 +0500

    Returing file on requests

--- Working with Express/app.js
+++ Working with Express/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParse = require('body-parser');
+const path = require('path');
 
 const adminRoute = require('./routes/admin');
 const shopRoute = require('./routes/shop');
@@ -13,7 +14,7 @@ app.use(shopRoute);
 
 // '/' is the default route if not specified
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
