00d984c759cfed96b456d0f539f9af1169f10862
Danish Riaz <danishriaz0609@outlook.com>
2020-06-02 08:43:12 +0500

    Using Express Router

--- Working with Express/app.js
+++ Working with Express/app.js
@@ -1,26 +1,15 @@
 const express = require('express');
 const bodyParse = require('body-parser');
 
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
+
 const app = express();
 
 app.use(bodyParse.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-    console.log('Inside add-product Middleware !');
-    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>');
-    res.end();
-});
-// app.use run for both incoming 'GET' & 'POST' request
-//we can use app.get OR app.post to make it handle only one
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    console.log('Inside / Middleware !');
-    res.send('<h1>Hello From Express</h1>');
-});
+app.use(adminRoute);
+app.use(shopRoute);
 
 app.listen(3000);
 
