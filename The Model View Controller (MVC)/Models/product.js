ef418d5c797e3d78b42377459c31b14d0e43ecb8
Danish Riaz <danishriaz0609@outlook.com>
2020-06-03 12:30:49 +0500

    Done with MVC

--- The Model View Controller (MVC)/Models/product.js
+++ The Model View Controller (MVC)/Models/product.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
 module.exports = class Product {
     constructor(title) {
         this.title = title
     }
 
     save() {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
         fs.readFile(p, (error, fileContent) => {
             let products = [];
             if (!error) {
@@ -21,7 +21,6 @@ module.exports = class Product {
     }
 
     static fetchAll(cb) {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
         fs.readFile(p, (err, fileContent) => {
             if (err) {
                 cb([]);
