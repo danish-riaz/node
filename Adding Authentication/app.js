5d895d4260bc0d953608c1f104234d1f267ea88c
Danish Riaz <danishriaz0609@outlook.com>
2020-06-06 17:36:52 +0500

    csrf Token added

--- Adding Authentication/app.js
+++ Adding Authentication/app.js
@@ -5,12 +5,13 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI = 'mongodb+srv://nodeapp:oJGOQJLwMbKlIM5Y@nodecompleteguide-rozde.mongodb.net/shop';
-
+const MONGODB_URI = 'mongodb+srv://nodeapp:ZZT6nzO3gKTVHaUs@nodecompleteguide-rozde.mongodb.net/shop';
+const csrfToken = csrf();
 
 const app = express();
 const store = new MongoDBStore({
@@ -35,6 +36,7 @@ app.use(
     store: store
   })
 );
+app.use(csrfToken);
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -46,6 +48,12 @@ app.use((req, res, next) => {
       next();
     })
     .catch(err => console.log(err));
+});
+
+app.use((req, res, next) => {
+  res.locals.csrfToken = req.csrfToken();
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  next();
 });
 
 app.use('/admin', adminRoutes);
